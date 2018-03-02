823c3473f93066538675521142747f0389c67824
Darren Kulp <darren@kulp.ch>
2018-03-02 13:47:36 -0700

    Skeleton of CLI [de]mangler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io::{self, BufRead};
+
+fn hexify(bytes : &str) -> String {
+    let mut out = String::new();
+
+    return out;
 }
+
+fn dehexify(bytes : &str, len : usize) -> String {
+    let mut out = String::new();
+
+    return out;
+}
+
+fn mangle(name : &str) -> String {
+    return String::from(name)
+}
+
+fn demangle(name : &str) -> String {
+    return String::from(name)
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let args : Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Need `--mangle` or `--demangle` option");
+    }
+    let func = match &args[1][..] {
+        "--mangle" => mangle,
+        "--demangle" => demangle,
+        _ => panic!("Invalid option `{}`", &args[1]),
+    };
+    for line in stdin.lock().lines() {
+        println!("{}", func(&line.unwrap()));
+    }
+}
+
