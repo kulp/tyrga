51a202b662eff40ad7d2092157439130c793b975
Darren Kulp <darren@kulp.ch>
2018-03-03 15:53:56 -0700

    Add stringifier for constant pool

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate regex;
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate classfile_parser;
 
+use classfile_parser::constant_info::*;
 use regex::Regex;
 use std::env;
 use std::io::{self, BufRead};
@@ -106,6 +109,32 @@ fn demangle(name : &str) -> String { // TODO Option<String>
 
     out.shrink_to_fit();
     return out;
+}
+
+fn stringify(pool : &Vec<ConstantInfo>, index : u16) -> Result<String,&str> {
+    let ci = &pool[(index - 1) as usize];
+    let st = stringify;
+    let p = pool;
+
+    use classfile_parser::constant_info::ConstantInfo::*;
+    return match *ci {
+        Utf8(ref x)               => Ok(x.utf8_string.clone()),
+        Integer(ref x)            => Ok(format!("{}", x.value)),
+        Float(ref x)              => Ok(format!("{}", x.value)),
+        Long(ref x)               => Ok(format!("{}", x.value)),
+        Double(ref x)             => Ok(format!("{}", x.value)),
+        Class(ref x)              => st(&p, x.name_index),
+        String(ref x)             => st(&p, x.string_index),
+        FieldRef(ref x)           => Ok(st(&p, x.class_index)? + "." + &st(&p, x.name_and_type_index)?),
+        MethodRef(ref x)          => Ok(st(&p, x.class_index)? + "." + &st(&p, x.name_and_type_index)?),
+        InterfaceMethodRef(ref x) => Ok(st(&p, x.class_index)? + "." + &st(&p, x.name_and_type_index)?),
+        NameAndType(ref x)        => Ok(st(&p, x.name_index )? + ":" + &st(&p, x.descriptor_index)?),
+        //MethodHandle
+        MethodType(ref x)         => st(&p, x.descriptor_index),
+        //InvokeDynamic
+
+        _ => Err("Unsupported constant p item type"),
+    };
 }
 
 fn main() {
