9b9fa21188011ccc8e2996371162acb8c3565206
Darren Kulp <darren@kulp.ch>
2019-02-27 17:24:15 -0700

    Add Opcode type

--- src/tenyr.rs
+++ src/tenyr.rs
@@ -14,3 +14,16 @@ impl fmt::Display for Register {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Opcode {
+    BitwiseOr       = 0x0, BitwiseOrn       = 0x8,
+    BitwiseAnd      = 0x1, BitwiseAndn      = 0x9,
+    BitwiseXor      = 0x2, Pack             = 0xa,
+    ShiftRightArith = 0x3, ShiftRightLogic  = 0xb,
+
+    Add             = 0x4, Subtract         = 0xc,
+    Multiply        = 0x5, ShiftLeft        = 0xd,
+    CompareEq       = 0x6, TestBit          = 0xe,
+    CompareLt       = 0x7, CompareGe        = 0xf,
+}
+
