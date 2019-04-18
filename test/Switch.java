7c7f439674b1f2842bf580786f8fd71e3c1eb84c
Darren Kulp <darren@kulp.ch>
2019-04-18 15:06:40 -0700

    Demo both Lookup and Table switches

--- test/Switch.java
+++ test/Switch.java
@@ -1,9 +1,18 @@
 public class Switch {
-    public static int beech(int i, int j) {
+    public static int table(int i, int j) {
         switch (i) {
             case 1: return j;
             case 2: return i * j;
             case 3: return i;
+            default: return 0;
+        }
+    }
+
+    public static int lookup(int i, int j) {
+        switch (i) {
+            case 1  : return j;
+            case 10 : return i * j;
+            case 100: return i;
             default: return 0;
         }
     }
