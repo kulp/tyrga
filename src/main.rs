kulp/tyrga

commit a2e47c361f436f5b5dbdaf5b70b2325bdff052b3

Generalize Yield handling

src/main.rs
@@ -167,14 +167,35 @@ fn make_instructions(sm : &mut StackManager, (addr, op) : (&usize, &Operation), 
             let OperandLocation::Register(z) = sm.get(0);
             (addr.clone(), vec![ Instruction { kind, z, x : Register::A, dd : NoLoad } ], default_dest)
         },
-        Yield { kind : JType::Void } => {
+        Yield { kind } => {
+            let ret = Instruction { kind : Type3(pos1_20), ..make_load(Register::P, stack_ptr) };
+            // TODO how to correctly place stack pointer ?
+            // StackManager will somehow have to help us manipulate it because we do not
+            // here have enough context otherwise.
+
+            use JType::*;
+            let v = match kind {
+                Void => {
+                    vec![ ret ]
+                },
+                Int | Float | Object | Short | Char | Byte => {
+                    let OperandLocation::Register(top) = sm.get(0);
+                    vec![
+                        make_store(frame_ptr, top),
+                        ret
+                    ]
+                },
+                Double | Long => {
+                    let OperandLocation::Register(top) = sm.get(0);
+                    let OperandLocation::Register(sec) = sm.get(1);
+                    vec![
+                        make_store(frame_ptr, sec),
+                        Instruction { kind : Type3(neg1_20), ..make_store(frame_ptr, top) },
+                        ret
+                    ]
+                },
+            };
             sm.empty();
-            let v = vec![
-                    // TODO how to correctly place stack pointer ?
-                    // StackManager will somehow have to help us manipulate it because we do not
-                    // here have enough context otherwise.
-                    Instruction { kind : Type3(pos1_20), ..make_load(Register::P, stack_ptr) },
-                ];
             (addr.clone(), v, vec![ Destination::Return ])
         },
 

commit c7763e121fe196929f2caca80a81d79b758fb0bf

Mend argument order bug introduced in a2e47c36

src/main.rs
@@ -339,7 +339,7 @@ fn make_instructions(sm : &mut StackManager, (addr, op) : (&usize, &Operation), 
                 Int | Float | Object | Short | Char | Byte => {
                     let top = get_reg(sm.get(0));
                     vec![
-                        make_store(frame_ptr, top),
+                        make_store(top, frame_ptr),
                         ret
                     ]
                 },
@@ -347,8 +347,8 @@ fn make_instructions(sm : &mut StackManager, (addr, op) : (&usize, &Operation), 
                     let top = get_reg(sm.get(0));
                     let sec = get_reg(sm.get(1));
                     vec![
-                        make_store(frame_ptr, sec),
-                        Instruction { kind : Type3(neg1_20), ..make_store(frame_ptr, top) },
+                        make_store(sec, frame_ptr),
+                        Instruction { kind : Type3(neg1_20), ..make_store(top, frame_ptr) },
                         ret
                     ]
                 },