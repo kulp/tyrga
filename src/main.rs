62cd0873a32eed97cbd9dda69b1958f55e9551f1
Darren Kulp <darren@kulp.ch>
2019-04-29 19:38:59 -0700

    Hoist make_call closure

--- src/main.rs
+++ src/main.rs
@@ -133,6 +133,34 @@ fn make_instructions(sm : &mut StackManager, (addr, op) : (&usize, &Operation), 
             jvmtypes::Comparison::Le => (tenyr::Opcode::CompareGe, true , false),
         };
 
+    let make_call = |sm : &mut StackManager, target, descriptor| {
+        let mut insns = Vec::new();
+        insns.extend(sm.freeze());
+
+        // Save return address into bottom of register-based stack
+        let bottom = Register::B; // TODO get bottom of StackManager instead
+        insns.push(Instruction {
+            kind : Type3(pos1_20.clone()),
+            ..make_mov(bottom, tenyr::Register::P)
+        });
+
+        let far = format!("@+{}", target);
+        insns.push(Instruction {
+            kind : Type3(tenyr::Immediate::Expr(exprtree::Atom::Variable(far))),
+            ..make_mov(tenyr::Register::P, tenyr::Register::P)
+        });
+
+        // adjust stack for returned values
+        let takes = count_args(descriptor);
+        let takes = takes.expect("failed to compute arguments size");
+        let rets = count_returns(descriptor);
+        let rets = rets.expect("failed to compute return size");
+        sm.release_frozen(u16::from(takes));
+        sm.reserve_frozen(u16::from(rets));
+        insns.extend(sm.thaw());
+        (*addr, insns, default_dest.clone())
+    };
+
     match op.clone() { // TODO obviate clone
         Constant { kind : JType::Int, value } => {
             let kind = Type3(Immediate20::new(value).expect("immediate too large"));
@@ -440,34 +468,7 @@ fn make_instructions(sm : &mut StackManager, (addr, op) : (&usize, &Operation), 
         // TODO fully handle Special (this is dumb partial handling)
         Invocation { kind : InvokeKind::Special, index } |
             Invocation { kind : InvokeKind::Static, index } =>
-        {
-            let mut insns = Vec::new();
-            insns.extend(sm.freeze());
-
-            // Save return address into bottom of register-based stack
-            let bottom = Register::B; // TODO get bottom of StackManager instead
-            insns.push(Instruction {
-                kind : Type3(pos1_20),
-                ..make_mov(bottom, tenyr::Register::P)
-            });
-
-            let far = format!("@+{}", make_callable_name(get_constant, index));
-            insns.push(Instruction {
-                kind : Type3(tenyr::Immediate::Expr(exprtree::Atom::Variable(far))),
-                ..make_mov(tenyr::Register::P, tenyr::Register::P)
-            });
-
-            // adjust stack for returned values
-            let parts = get_method_parts(get_constant, index);
-            let takes = count_args(&parts.2);
-            let takes = takes.expect("failed to compute arguments size");
-            let rets = count_returns(&parts.2);
-            let rets = rets.expect("failed to compute return size");
-            sm.release_frozen(u16::from(takes));
-            sm.reserve_frozen(u16::from(rets));
-            insns.extend(sm.thaw());
-            (*addr, insns, default_dest)
-        },
+            make_call(sm, &make_callable_name(get_constant, index), &get_method_parts(get_constant, index).2),
 
         _ => panic!("unhandled operation {:?}", op),
     }
