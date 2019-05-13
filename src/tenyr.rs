e52bbb746f44851c5a8e4605a5391f364fcd42a6
Darren Kulp <darren@kulp.ch>
2019-05-13 16:55:47 -0700

    Make op and second register optional

--- src/tenyr.rs
+++ src/tenyr.rs
@@ -66,12 +66,12 @@ macro_rules! tenyr_insn {
             result
         }
     };
-    ( $z:ident <- $x:ident $op:tt $y:ident $( + $imm:expr )? ) => {
+    ( $z:ident <- $x:ident $( $op:tt $y:ident $( + $imm:expr )? )? ) => {
         {
             use $crate::tenyr::*;
-            use std::convert::TryInto;
+            #[allow(unused_imports)] use std::convert::TryInto;
             let gen = InsnGeneral { y : Register::A, op : Opcode::BitwiseOr, imm : 0u8.into() };
-            let kind = Type0(InsnGeneral { y : Register::$y, op : tenyr_op!($op), $( imm : $imm.try_into().map_err::<Box<std::error::Error>,_>(Into::into)?, )? ..gen });
+            let kind = Type0(InsnGeneral { $( y : Register::$y, op : tenyr_op!($op), $( imm : $imm.try_into().map_err::<Box<std::error::Error>,_>(Into::into)?, )? )? ..gen });
             let result : Result<_, Box<std::error::Error>> = Ok(Instruction { kind, z : Register::$z, x : Register::$x, dd : MemoryOpType::NoLoad });
             result
         }
@@ -98,6 +98,7 @@ fn test_macro_insn() -> Result<(), Box<std::error::Error>> {
     assert_eq!(tenyr_insn!(B <- C * D + 3).unwrap(), Instruction { kind : Type0(InsnGeneral { y : D, op : Multiply, imm : 3u8.into() }), z : B, x : C, dd : NoLoad });
     assert_eq!(tenyr_insn!(B <- C + 3).unwrap(), Instruction { kind : Type0(InsnGeneral { y : A, op : BitwiseOr, imm : 3u8.into() }), z : B, x : C, dd : NoLoad });
     assert_eq!(tenyr_insn!(B <- 3).unwrap(), Instruction { kind : Type3(3u8.into()), z : B, x : A, dd : NoLoad });
+    assert_eq!(tenyr_insn!(B <- C * D).unwrap(), Instruction { kind : Type0(InsnGeneral { y : D, op : Multiply, imm : 0u8.into() }), z : B, x : C, dd : NoLoad });
 
     Ok(())
 }
