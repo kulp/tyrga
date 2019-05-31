2f290a5f13cc208834fbbba4b13f69517ac65d26
Darren Kulp <darren@kulp.ch>
2019-05-31 16:07:54 -0700

    Copy any test/*.tas into $OUT_DIR/

--- build.rs
+++ build.rs
@@ -11,11 +11,11 @@ fn main() -> Result<(), Box<Error>> {
     for entry in fs::read_dir(Path::new(test_dir))? {
         let path = entry?.path();
         let name = &path.to_str().ok_or("path is not a UTF-8 string")?;
-        if let Some(e) = path.extension() {
-            if e == "java" {
-                let stem = &path.file_stem().ok_or("filename is empty")?;
+        let stem = &path.file_stem().ok_or("filename is empty")?;
+        let source_meta = &path.metadata()?;
+        match path.extension() {
+            Some(e) if e == "java" => {
                 let dest = out_path.join(stem).with_extension("class");
-                let source_meta = &path.metadata()?;
                 // run javac only if files need to be updated
                 if !dest.exists() || source_meta.modified()? > dest.metadata()?.modified()? {
                     Command::new("javac")
@@ -24,7 +24,16 @@ fn main() -> Result<(), Box<Error>> {
                         .arg(name)
                         .status()?;
                 }
-            }
+            },
+            Some(e) if e == "tas" => {
+                let dest = out_path.join(path.file_name().ok_or("filename is empty")?);
+                // copy file into build directory so its location can be referenced with
+                // env!("OUT_DIR")
+                fs::copy(path, dest)?;
+            },
+            _ => {
+                eprintln!("Skipping unrecognized file {}", path.display());
+            },
         }
     }
 
