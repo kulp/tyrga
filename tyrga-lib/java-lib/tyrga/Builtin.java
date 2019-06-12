2eb9955dfbac316001e83ff2e391c83de34ab200
Darren Kulp <darren@kulp.ch>
2019-06-11 20:45:36 -0700

    Simplify logic for Builtin.rem(int, int)

--- tyrga-lib/java-lib/tyrga/Builtin.java
+++ tyrga-lib/java-lib/tyrga/Builtin.java
@@ -5,24 +5,20 @@ public class Builtin {
         if (b == 0)
             return a / 0; // defer to division by zero
 
-        if (a < 0) {
-            if (b < 0) {
-                return -rem(-a, -b);
-            } else {
-                return -rem(-a,  b);
-            }
-        } else if (b < 0) {
-            return rem(a, -b);
-        } else {
-            if (b == 1)
-                return 0;
-            if (b == 2)
-                return a & 1;
+        if (a < 0)
+            return -rem(-a, b);
 
-            while (a >= b)
-                a -= b;
-            return a;
-        }
+        if (b < 0)
+            b = -b;
+
+        if (b == 1)
+            return 0;
+        if (b == 2)
+            return a & 1;
+
+        while (a >= b)
+            a -= b;
+        return a;
     }
 }
 
