3c8a15f681657df265f246d05aae951f766aacac
Darren Kulp <darren@kulp.ch>
2019-06-12 17:07:35 -0700

    Handle boundary conditions

--- tyrga-lib/java-lib/tyrga/Builtin.java
+++ tyrga-lib/java-lib/tyrga/Builtin.java
@@ -5,19 +5,21 @@ public class Builtin {
         if (b == 0)
             return a / 0; // defer to division by zero
 
-        if (a < 0)
-            return -rem(-a, b);
-
-        if (b < 0)
-            b = -b;
-
+        // Optimizations
         if (b == 1)
             return 0;
-        if (b == 2)
-            return a & 1;
 
-        while (a >= b)
+        if (b > 0)
+            b = -b;
+
+        // do modulus on negative numbers, because in two's-complement, those
+        // can be larger in magnitude than positive numbers can
+        if (a > 0)
+            return -rem(-a, b);
+
+        while (a <= b)
             a -= b;
+
         return a;
     }
 }
