7914abdbd1275e1e584af412a03f0fb0c4288f1b
Darren Kulp <darren@kulp.ch>
2019-06-17 17:05:24 -0700

    Add and test Builtin.div(int, int)

--- tyrga-lib/java-lib/tyrga/Builtin.java
+++ tyrga-lib/java-lib/tyrga/Builtin.java
@@ -1,6 +1,32 @@
 package tyrga;
 
 public class Builtin {
+    public static int div(int a, int b) {
+        if (b == 0)
+            return (1 << 32) - 1; // TODO handle how ?
+
+        // Optimizations
+        if (b == 1)
+            return a;
+
+        boolean flip = false;
+        int c = 0;
+        if (b > 0) {
+            flip = ! flip;
+            b = -b;
+        }
+
+        // operate on negative numbers, because in two's-complement, those can
+        // be larger in magnitude than positive numbers can
+        if (a > 0)
+            return -div(-a, b);
+
+        for ( ; a <= b; a -= b)
+            c++;
+
+        return flip ? -c : c;
+    }
+
     public static int rem(int a, int b) {
         if (b == 0)
             return a / 0; // defer to division by zero
@@ -25,6 +51,40 @@ public class Builtin {
 }
 
 class BuiltinTest {
+    private static boolean try_div_int(int a, int b) {
+        int expected = a / b;
+        int got = Builtin.div(a, b);
+        return got == expected;
+    }
+
+    public static boolean test_div_int() {
+        int smallest = -(1 << 31);
+        int largest = (1 << 31) - 1;
+        int cases[] = {
+            smallest, smallest + 1, -100, -10, -3, -2, -1, 1, 2, 3, 10, 100, largest - 1, largest
+        };
+        for (int i = 0; i < cases.length; i++) {
+            for (int j = 0; j < cases.length; j++) {
+                if (! try_div_int(cases[i], cases[j])) {
+                    return false;
+                }
+            }
+        }
+
+        for (int a = -100; a < 100; a++) {
+            for (int b = -100; b < 100; b++) {
+                if (b == 0)
+                    continue;
+                if (! try_div_int(a, b)) {
+                    System.err.println("a=" + a + ", b=" + b);
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     private static boolean try_rem_int(int a, int b) {
         int expected = a % b;
         int got = Builtin.rem(a, b);
@@ -63,6 +123,7 @@ class BuiltinTest {
         boolean failed = false;
 
         failed |= !test_rem_int();
+        failed |= !test_div_int();
 
         System.exit(failed ? 1 : 0);
     }
