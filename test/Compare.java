1b49bc942c0af0c4e3acf8442c2c96e2775290c5
Darren Kulp <darren@kulp.ch>
2019-06-22 05:54:24 -0700

    Add "-or-equal" operator tests

--- test/Compare.java
+++ test/Compare.java
@@ -1,10 +1,16 @@
 public class Compare {
-    public static boolean lt(float  a, float  b) { return a < b; }
-    public static boolean gt(float  a, float  b) { return a > b; }
+    public static boolean lt(float  a, float  b) { return a <  b; }
+    public static boolean le(float  a, float  b) { return a <= b; }
+    public static boolean gt(float  a, float  b) { return a >  b; }
+    public static boolean ge(float  a, float  b) { return a >= b; }
 
-    public static boolean lt(double a, double b) { return a < b; }
-    public static boolean gt(double a, double b) { return a > b; }
+    public static boolean lt(double a, double b) { return a <  b; }
+    public static boolean le(double a, double b) { return a <= b; }
+    public static boolean gt(double a, double b) { return a >  b; }
+    public static boolean ge(double a, double b) { return a >= b; }
 
-    public static boolean lt(long   a, long   b) { return a < b; }
-    public static boolean gt(long   a, long   b) { return a > b; }
+    public static boolean lt(long   a, long   b) { return a <  b; }
+    public static boolean le(long   a, long   b) { return a <= b; }
+    public static boolean gt(long   a, long   b) { return a >  b; }
+    public static boolean ge(long   a, long   b) { return a >= b; }
 }
