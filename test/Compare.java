0f24c305bf6b0b2d909ea4b05fdb084c86515097
Darren Kulp <darren@kulp.ch>
2019-06-22 05:51:45 -0700

    Reduce vertical whitespace in Compare.java

--- test/Compare.java
+++ test/Compare.java
@@ -1,25 +1,10 @@
 public class Compare {
-    public static boolean lessThan(float a, float b) {
-        return a < b;
-    }
+    public static boolean lessThan(float  a, float  b) { return a < b; }
+    public static boolean moreThan(float  a, float  b) { return a > b; }
 
-    public static boolean moreThan(float a, float b) {
-        return a > b;
-    }
+    public static boolean lessThan(double a, double b) { return a < b; }
+    public static boolean moreThan(double a, double b) { return a > b; }
 
-    public static boolean lessThan(double a, double b) {
-        return a < b;
-    }
-
-    public static boolean moreThan(double a, double b) {
-        return a > b;
-    }
-
-    public static boolean lessThan(long a, long b) {
-        return a < b;
-    }
-
-    public static boolean moreThan(long a, long b) {
-        return a > b;
-    }
+    public static boolean lessThan(long   a, long   b) { return a < b; }
+    public static boolean moreThan(long   a, long   b) { return a > b; }
 }
