a72c564473d3d1fc479972c95883a7a0165faba5
Darren Kulp <darren@kulp.ch>
2019-06-22 05:56:27 -0700

    Reduce vertical whitespace in Allocate.java

--- test/Allocate.java
+++ test/Allocate.java
@@ -1,33 +1,10 @@
 public class Allocate {
-    public static boolean[] array_allocate_boolean() {
-        return new boolean[10];
-    }
-
-    public static char[] array_allocate_char() {
-        return new char[10];
-    }
-
-    public static float[] array_allocate_float() {
-        return new float[10];
-    }
-
-    public static double[] array_allocate_double() {
-        return new double[5];
-    }
-
-    public static byte[] array_allocate_byte() {
-        return new byte[10];
-    }
-
-    public static short[] array_allocate_short() {
-        return new short[10];
-    }
-
-    public static int[] array_allocate_int() {
-        return new int[10];
-    }
-
-    public static long[] array_allocate_long() {
-        return new long[5];
-    }
+    public static boolean [] array_allocate_boolean() { return new boolean [10]; }
+    public static char    [] array_allocate_char()    { return new char    [10]; }
+    public static float   [] array_allocate_float()   { return new float   [10]; }
+    public static double  [] array_allocate_double()  { return new double  [5] ; }
+    public static byte    [] array_allocate_byte()    { return new byte    [10]; }
+    public static short   [] array_allocate_short()   { return new short   [10]; }
+    public static int     [] array_allocate_int()     { return new int     [10]; }
+    public static long    [] array_allocate_long()    { return new long    [5] ; }
 }
