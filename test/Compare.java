ee0ad4311ae1eea8d13dbd3975504f647cba604e
Darren Kulp <darren@kulp.ch>
2019-06-23 05:45:37 -0700

    Add `eq` and `ne` comparisons to Compare testcase

--- test/Compare.java
+++ test/Compare.java
@@ -1,16 +1,22 @@
 public class Compare {
     public static boolean lt(float  a, float  b) { return a <  b; }
     public static boolean le(float  a, float  b) { return a <= b; }
+    public static boolean eq(float  a, float  b) { return a == b; }
     public static boolean gt(float  a, float  b) { return a >  b; }
     public static boolean ge(float  a, float  b) { return a >= b; }
+    public static boolean ne(float  a, float  b) { return a != b; }
 
     public static boolean lt(double a, double b) { return a <  b; }
     public static boolean le(double a, double b) { return a <= b; }
+    public static boolean eq(double a, double b) { return a == b; }
     public static boolean gt(double a, double b) { return a >  b; }
     public static boolean ge(double a, double b) { return a >= b; }
+    public static boolean ne(double a, double b) { return a != b; }
 
     public static boolean lt(long   a, long   b) { return a <  b; }
     public static boolean le(long   a, long   b) { return a <= b; }
+    public static boolean eq(long   a, long   b) { return a == b; }
     public static boolean gt(long   a, long   b) { return a >  b; }
     public static boolean ge(long   a, long   b) { return a >= b; }
+    public static boolean ne(long   a, long   b) { return a != b; }
 }
