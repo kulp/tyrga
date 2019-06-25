4f963bdbe2205fae68f6a387bad02baf14b787a2
Darren Kulp <darren@kulp.ch>
2019-06-25 16:49:42 -0700

    Add testcase for Constant

---
+++ test/Constant.java
@@ -0,0 +1,7 @@
+public class Constant {
+    public static int    constant_int()     { return 1; }
+    public static long   constant_long()    { return 1; }
+    public static float  constant_float()   { return 1; }
+    public static double constant_double()  { return 1; }
+    public static Object constant_Object()  { return null; }
+}
