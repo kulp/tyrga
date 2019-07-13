c12d6513d99a8cfd5d407f49dc0648d8483f3ba9
Darren Kulp <darren@kulp.ch>
2019-07-13 09:31:26 -0700

    Introduce make_name_in_class

--- tyrga-lib/src/lib.rs
+++ tyrga-lib/src/lib.rs
@@ -866,22 +866,29 @@ fn make_callable_name(get_constant : &ConstantGetter, pool_index : u16) -> Gener
     mangling::mangle(joined.bytes())
 }
 
-fn make_mangled_name<T>(class : &ClassFile, item : &T) -> GeneralResult<String>
-    where T : Named + Described
-{
+fn make_name_in_class(class : &ClassFile, pieces : &[&dyn std::fmt::Display]) -> GeneralResult<String> {
     let get_constant = get_constant_getter(class);
 
     match get_constant(class.this_class) {
         classfile_parser::constant_info::ConstantInfo::Class(cl) => {
-            let get_string = |&n|
-                Ok(get_string(&get_constant, n).ok_or("no such string in constant pool")?);
-
-            let arr = [ cl.name_index, item.name_index(), item.descriptor_index() ];
-            let got : GeneralResult<Vec<_>> = arr.iter().map(get_string).collect();
-            mangling::mangle(got?.join(":").bytes())
+            let s = get_string(&get_constant, cl.name_index).ok_or("no such string in constant pool")?;
+            let got : Vec<_> = [&s as &dyn std::fmt::Display].iter().chain(pieces).map(ToString::to_string).collect();
+            mangling::mangle(got.join(":").bytes())
         },
         _ => Err("not a class".into()),
     }
+}
+
+fn make_mangled_name<T>(class : &ClassFile, item : &T) -> GeneralResult<String>
+    where T : Named + Described
+{
+    let get_constant = get_constant_getter(class);
+    let get_string = |n| get_string(&get_constant, n);
+    let arr : &[&dyn std::fmt::Display] = &[
+            &get_string(item.name_index()).ok_or("missing name")?,
+            &get_string(item.descriptor_index()).ok_or("missing descriptor")?,
+        ];
+    make_name_in_class(class, &arr)
 }
 
 fn make_label(class : &ClassFile, method : &MethodInfo, suffix : &str) -> GeneralResult<String> {
