edde907ed3c54c7f563ae0f34c64dab2008d06f9
Darren Kulp <darren@kulp.ch>
2019-07-30 17:07:17 -0700

    Make test failure output easier to read

--- tyrga-bin/src/main.rs
+++ tyrga-bin/src/main.rs
@@ -40,7 +40,7 @@ fn test_translate_file() -> TerminatingResult {
             let mut expected = Vec::new();
             File::open(gold)?.read_to_end(&mut expected)?;
 
-            assert_eq!(translated, expected);
+            assert_eq!(&std::str::from_utf8(&translated), &std::str::from_utf8(&expected));
         }
     }
 
