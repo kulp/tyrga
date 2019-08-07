f4745af5f6d398655c29b39325a678c50b5f17d2
Darren Kulp <darren@kulp.ch>
2019-08-06 18:01:31 -0700

    Implement Manglable for MethodRefConstant

--- tyrga-lib/src/lib.rs
+++ tyrga-lib/src/lib.rs
@@ -92,6 +92,27 @@ impl Manglable for Context<'_, &FieldRefConstant> {
     }
 }
 
+// TODO deduplicate code with FieldRefConstant above
+impl Manglable for Context<'_, &MethodRefConstant> {
+    fn pieces(&self) -> GeneralResult<Vec<std::string::String>> {
+        use classfile_parser::constant_info::ConstantInfo::*;
+
+        let fr = self.as_ref();
+        if let Class(ni) = self.get_constant(fr.class_index) {
+            let ni = ni.name_index;
+            let ss = get_string(self, ni).ok_or("no such name")?;
+            if let NameAndType(nt) = self.get_constant(fr.name_and_type_index) {
+                let nt = self.contextualize(nt);
+                Ok(std::iter::once(ss).chain(nt.pieces()?.into_iter()).collect())
+            } else {
+                Err("invalid ConstantInfo kind".into())
+            }
+        } else {
+            Err("invalid ConstantInfo kind".into())
+        }
+    }
+}
+
 fn expand_immediate_load(sm : &mut stack::Manager, insn : Instruction, imm : i32)
     -> GeneralResult<Vec<Instruction>>
 {
