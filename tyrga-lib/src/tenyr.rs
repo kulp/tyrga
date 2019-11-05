6ab73a8e001008144042a9e60fa794091308a1a5
Darren Kulp <darren@kulp.ch>
2019-11-05 16:55:30 -0700

    Prefer empty iterator over empty vector for base case

--- tyrga-lib/src/tenyr.rs
+++ tyrga-lib/src/tenyr.rs
@@ -272,7 +272,7 @@ fn test_macro_insn() -> Result<(), Box<dyn std::error::Error>> {
 /// Parses a list of tenyr instructions, each terminated with `;`, into a chained `Iterator`
 #[macro_export]
 macro_rules! tenyr_insn_list {
-    () => { vec![] };
+    () => { std::iter::empty() };
     ( $lhs:tt <- $a:tt $op:tt$op2:tt $b:tt $( + $c:tt )? ; $( $tok:tt )* ) => { std::iter::once(tenyr_insn!($lhs <- $a $op$op2 $b $( + $c )?  )?).chain(tenyr_insn_list!($( $tok )*)) };
     ( $lhs:tt <- $a:tt $op:tt        $b:tt $( + $c:tt )? ; $( $tok:tt )* ) => { std::iter::once(tenyr_insn!($lhs <- $a $op     $b $( + $c )?  )?).chain(tenyr_insn_list!($( $tok )*)) };
     ( $lhs:tt <- $a:tt $op:tt$op2:tt $b:tt $( - $c:tt )? ; $( $tok:tt )* ) => { std::iter::once(tenyr_insn!($lhs <- $a $op$op2 $b $( - $c )?  )?).chain(tenyr_insn_list!($( $tok )*)) };
